500ebaae7b041655f17959b8dfaba193f48b7265
jonathankim407 <jonathankim407@gmail.com>
2017-06-21 16:57:44 -0400

    first commit

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,4 +3,11 @@ class Pledge < ApplicationRecord
   belongs_to :project
 
   validates :dollar_amount, presence: true
+  validate :abra_cadaver
+
+  def abra_cadaver
+    if user_id == project.owner_id
+      errors.add(:user, "cannot pledge as owner.")
+    end
+   end
 end
